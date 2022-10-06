918d0dce56ef6374d2ca18a056b2660aaf094236
Myzwer <josh@onefortyfivedesign.com>
2022-10-06 14:49:41 -0400

    :art: Moved the text out of default into index, and made the video swap places on mobile to be the "header."

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,15 +5,10 @@
         <img
           src="/images/logo.png"
           alt="marker"
-          class="text-center mx-auto max-w-xs p-5"
+          class="text-center mx-auto fc-logo p-1"
         >
       </nuxt-link>
     </header>
-    <div class="py-14 md:max-w-5xl text-center mx-auto">
-          <h1 class="uppercase font-bold text-4xl">Foothills Church Vision Offering</h1>
-          <p class="bold text-2xl">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A natus officia quibusdam
-            sed similique. Architecto aspernatur, assumenda delectus eius et.</p>
-    </div>
     <Nuxt/>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,22 @@
   <div>
     <div class="text-center mx-auto">
       <div class="mx-auto text-center lg:max-w-7xl pb-10">
-        <div class="grid grid-cols-12 gap-4 px-5">
-          <div class="col-span-12 md:col-span-6">
+        <div class="grid grid-cols-12 gap-4 md:px-5">
+
+          <div class="col-span-12 md:col-span-6 md:order-2">
             <VideoPlayer />
           </div>
-          <div class="col-span-12 md:col-span-6">
+
+          <div class="col-span-12 md:order-1">
+            <div class="mt-2 md:pt-14 pb-14 p-2 text-left md:text-center mx-auto">
+              <h1 class="uppercase font-bold text-4xl">Foothills Church Vision Offering</h1>
+              <p class="bold text-2xl">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A natus officia quibusdam
+                sed similique. Architecto aspernatur, assumenda delectus eius et.</p>
+            </div>
+          </div>
+
+
+          <div class="col-span-12 md:col-span-6 md:order-3">
             <div class="px-2 lg:px-10">
             <ProgressBar />
             <GivingData />
