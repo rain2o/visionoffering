d9aec746f4b79e8eb79da9f28cdeebe94d823ef3
Myzwer <josh@onefortyfivedesign.com>
2022-10-11 16:55:24 -0400

    :construction: Working on the design and functionality of the campaign story component.

--- components/CampaignTab.vue
+++ components/CampaignTab.vue
@@ -1,11 +1,48 @@
 <template>
-  <div>
-    <h1>Campaign Data</h1>
+  <div class="lg:max-w-2xl py-10">
+    <div class="grid grid-cols-12 gap-4">
+
+      <div class="col-span-12">
+        <h3 class=" uppercase bold text-2xl pb-5">Why we give</h3>
+                <img
+                  src="~/static/images/campaign/worship.jpg"
+                  alt="icon"
+                  class=""
+                >
+      </div>
+
+      <div class="col-span-12 pt-10">
+        <h3 class=" uppercase bold text-2xl">Another Title</h3>
+        <p class="text-xl">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam at delectus, distinctio
+          dolor dolore eos
+          esse excepturi hic laborum maxime porro quae quam quasi repellendus sequi, sint sunt tenetur, velit?</p>
+      </div>
+    </div>
+
+
+    <div class="col-span-12 pt-10">
+      <img
+        src="~/static/images/campaign/goal-1.jpg"
+        alt="icon"
+        class=""
+      >
+    </div>
+    <div class="col-span-12 pt-10">
+      <h3 class=" uppercase bold text-2xl">We can change this city batman</h3>
+      <p class="text-xl">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam at delectus, distinctio
+        dolor dolore eos
+        esse excepturi hic laborum maxime porro quae quam quasi repellendus sequi, sint sunt tenetur, velit?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam at delectus, distinctio
+        dolor dolore eos
+        esse excepturi hic laborum maxime porro quae quam quasi repellendus sequi, sint sunt tenetur, velit?</p>
+    </div>
+
   </div>
+
 </template>
 
 <script>
 export default {
-  name: 'CampaignData'
+  name: 'CampaignData',
 }
 </script>
