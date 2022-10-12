cfb3d2b8393d41c0afc7528473b8dcbd07d749b2
Myzwer <josh@onefortyfivedesign.com>
2022-10-12 10:15:47 -0400

    :rocket: Finished the base layout for the FAQ section.

--- components/FaqTab.vue
+++ components/FaqTab.vue
@@ -1,7 +1,47 @@
 <template>
-  <div>
-    <h1>FAQ Data</h1>
+
+  <div class="lg:max-w-2xl py-10">
+    <div class="grid grid-cols-12 gap-4">
+      <div class="col-span-12">
+        <h3 class=" uppercase bold text-2xl pb-5">FAQ</h3>
+        <hr>
+        <div class="py-5">
+          <h5 class = "uppercase font-bold text-xl">Why should I give any money to this at all?</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur dolor, eaque impedit itaque, iusto
+            libero, pariatur porro quidem reiciendis rem rerum tempore! Explicabo, officiis, ut! Assumenda et incidunt
+            iure veniam?</p>
+        </div>
+
+        <hr>
+        <div class="py-5">
+          <h5 class = "uppercase font-bold text-xl">Why should I trust you when your comms guy can't even listen to the right podcast?</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur dolor, eaque impedit itaque, iusto
+            libero, pariatur porro quidem reiciendis rem rerum tempore! Explicabo, officiis, ut! Assumenda et incidunt
+            iure veniam?</p>
+        </div>
+
+        <hr>
+        <div class="py-5">
+          <h5 class = "uppercase font-bold text-xl">Is this the fall classic combat website?</h5>
+          <p>Literally no.</p>
+        </div>
+
+        <hr>
+        <div class="py-5">
+          <h5 class = "uppercase font-bold text-xl">How much should I give?</h5>
+          <p>Enough to buy Jordan a new drone.</p>
+        </div>
+
+        <hr>
+        <div class="py-5 mb-32">
+          <h5 class = "uppercase font-bold text-xl">Do poeple even read these?</h5>
+          <p>Yes! If we are trying to get drone money for Jordan, people need to be sold on WHY they should give us a thousand dollars.</p>
+        </div>
+
+      </div>
+    </div>
   </div>
+
 </template>
 
 <script>
