e9ee44f371229cb95714b968eb43907ce44ff7d7
Myzwer <josh@onefortyfivedesign.com>
2022-10-12 10:50:55 -0400

    :rocket: Created the base layout for the update section.

--- components/UpdateTab.vue
+++ components/UpdateTab.vue
@@ -1,6 +1,35 @@
 <template>
-  <div>
-    <h1>Update Data</h1>
+  <div class="lg:max-w-2xl py-10">
+    <div class="grid grid-cols-12 gap-4">
+      <div class="col-span-12">
+        <h3 class=" uppercase bold text-2xl pb-5">Updates</h3>
+
+        <div class="py-5">
+          <img
+            src="~/static/images/updates/update-1.jpg"
+            alt="icon"
+            class="rounded-md shadow-2xl"
+          >
+          <h5 class="uppercase font-bold text-2xl py-5">Update #1</h5>
+          <hr>
+          <p class="py-3">Duis egestas pretium mauris, a consectetur quam tempor porta. Vivamus egestas nunc ut lacinia
+            hendrerit. Phasellus eu dui tellus. Sed ex elit, finibus ullamcorper consequat sit amet, tincidunt vitae
+            orci. Nulla mollis nisi orci, a consequat quam vehicula id. In tincidunt arcu diam, eget tempus sapien
+            euismod hendrerit. Curabitur lobortis risus non augue semper, eu malesuada nulla dictum. Vestibulum semper
+            diam ac orci ornare ornare. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac
+            turpis egestas. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis
+            egestas. Integer interdum nulla odio, varius fringilla dui feugiat et.</p>
+          <p class = "pb-3">We are doing these things now:</p>
+          <ul class = "font-bold list-disc list-inside">
+            <li>Do a certain thing</li>
+            <li>Get Jordan a drone</li>
+            <li>Profit</li>
+          </ul>
+          <p class = "py-3">Aliquam id augue id neque vulputate egestas a sed dolor. Quisque finibus finibus magna ac faucibus. Mauris sodales metus vel quam tempus, tincidunt imperdiet lectus vulputate. Nullam interdum at odio sit amet feugiat. Proin pulvinar, lectus ut mattis laoreet, metus sapien lacinia leo, ac vehicula augue metus sed dui. Aenean mollis in magna finibus imperdiet. Donec hendrerit tellus at felis blandit gravida. Maecenas sed elementum neque, sed rutrum odio. Nunc risus ante, consectetur in porttitor vitae, tempus sit amet erat. Praesent sit amet viverra ex, eget semper leo. Fusce cursus sapien vitae arcu ullamcorper sagittis. Fusce tellus elit, imperdiet vel erat sed, hendrerit fermentum leo. Nulla efficitur arcu hendrerit nulla porttitor viverra. Quisque ac urna at tortor aliquet porttitor non at mi.</p>
+        </div>
+
+      </div>
+    </div>
   </div>
 </template>
 
