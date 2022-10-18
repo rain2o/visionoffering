883c56e4ddd4f5eca4040bf109df1db0e9de107b
Myzwer <josh@onefortyfivedesign.com>
2022-10-18 14:58:13 -0400

    :repeat: Refactored these components to free up index from the future API data.

--- components/GivingData.vue
+++ components/GivingData.vue
@@ -1,29 +1,12 @@
 <template>
-  <div class="text-left">
-    <div class="py-3">
-      <h2 class="text-blue font-bold text-4xl">${{ Number(givenAmount).toLocaleString() }}</h2>
-      <p>Given of $200,000</p>
-    </div>
-
-    <div class="py-3">
-      <h2 class="font-bold text-4xl">387</h2>
-      <p>Givers</p>
-    </div>
-
-    <div class="py-3">
-      <h2 class="font-bold text-4xl">16</h2>
-      <p>Days to Go</p>
-    </div>
+  <div>
+    <ProgressBar />
+    <DisplayData />
   </div>
 </template>
 
 <script>
 export default {
   name: 'GivingData',
-  data() {
-    return {
-      givenAmount: "180000",
-    }
-  },
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -5,13 +5,14 @@
         <div class="grid grid-cols-12 gap-4 md:px-5">
 
           <div class="col-span-12 md:col-span-6 md:order-2">
-            <VideoPlayer />
+            <VideoPlayer/>
           </div>
 
           <div class="col-span-12 md:order-1">
             <div class="mt-2 md:pt-14 pb-14 p-2 text-left md:text-center mx-auto">
               <h1 class="uppercase font-bold text-4xl">Foothills Church Vision Offering</h1>
-              <p class="bold text-2xl">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A natus officia quibusdam
+              <p class="bold text-2xl">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A natus officia
+                quibusdam
                 sed similique. Architecto aspernatur, assumenda delectus eius et.</p>
             </div>
           </div>
@@ -19,15 +20,14 @@
 
           <div class="col-span-12 md:col-span-6 md:order-3">
             <div class="px-2 lg:px-10">
-            <ProgressBar />
-            <GivingData />
-            <XLButton />
+              <GivingData/>
+              <XLButton/>
             </div>
           </div>
         </div>
       </div>
 
-      <FullBanner />
+      <FullBanner/>
 
     </div>
 
@@ -47,9 +47,15 @@
 
 
           <v-tabs-items v-model="tab">
-            <v-tab-item key="Campaign"><CampaignTab /></v-tab-item>
-            <v-tab-item key="FAQ"><FaqTab /></v-tab-item>
-            <v-tab-item key="Updates"><UpdateTab /></v-tab-item>
+            <v-tab-item key="Campaign">
+              <CampaignTab/>
+            </v-tab-item>
+            <v-tab-item key="FAQ">
+              <FaqTab/>
+            </v-tab-item>
+            <v-tab-item key="Updates">
+              <UpdateTab/>
+            </v-tab-item>
           </v-tabs-items>
         </div>
       </div>
@@ -64,7 +70,7 @@ export default {
   data() {
     return {
       tab: null,
-   }
+    }
   },
 }
 </script>
