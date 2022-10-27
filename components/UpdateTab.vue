457ff1c567b3f43d5e400c34baa456a9b5186103
Myzwer <josh@onefortyfivedesign.com>
2022-10-27 11:18:00 -0400

    :scroll: Updated copy and swapped out the image.

--- components/UpdateTab.vue
+++ components/UpdateTab.vue
@@ -6,7 +6,7 @@
 
         <div class="py-5">
           <img
-            src="~/static/images/updates/update-1.jpg"
+            src="~/static/images/updates/update-initial.jpg"
             alt="icon"
             class="rounded-md shadow-2xl"
           >
@@ -17,18 +17,24 @@
             a one-time gift over and above our regular contributions to fuel God's vision. This year, we have three
             goals that we want to encourage you to pray for and give toward. </p>
 
-          <p class="py-3">First, we hope to raise <span class = "font-bold">$374,000</span> to
+          <p class="py-3">First, we hope to raise <span class="font-bold">$374,000</span> to
             renovate the new home of our Knoxville location. God provided the space, but we now need to turn a movie
             theater into a church. Second, we have a dream to start a counseling center to address the many spiritual
             and relational needs in our community. </p>
-          <p class="py-3">We believe that if we can raise <span class = "font-bold">$225,000</span>, we can take our next step
+          <p class="py-3">We believe that if we can raise <span class="font-bold">$225,000</span>, we can take our next
+            step
             to hire staff, purchase resources, and address the many start-up cost required with a new ministry. </p>
 
-          <p class="py-3">Finally, we are diligently pursuing property surrounding our Maryville location to provide space for more parking. We
-            believe we need to raise <span class = "font-bold">$505,000</span> in order to do this. With all three of these goals, the total monetary
-            goal is $1,100,000. That may sound like an extraordinary number to you. But God is not intimidated by that
-            number. We can do this! But it will take faith, sacrifice, and commitment to take our next step. Let’s give
-            to the Vision Offering and see what God will do.</p>
+          <p class="py-3">Finally, we are diligently pursuing property surrounding our Maryville location to provide
+            space for more parking. We
+            believe we need to raise <span class="font-bold">$505,000</span> in order to do this. With all three of
+            these goals, the total monetary
+            goal is $1,100,000. That may sound like an extraordinary number to you, but God is not intimidated by that
+            number. </p>
+
+          <p class="py-3"><span class = "font-bold">We can do this!</span> But it will take faith, sacrifice, and commitment to take our next step. Let’s give
+            to the Vision Offering by November 20th and see what God will do. You can prayerfully check back on this
+            page for updates on our goals and to see how God is using our generosity to fuel the vision.</p>
         </div>
 
       </div>
