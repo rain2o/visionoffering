8e075dad107503c2b537aec56c8fc1867a4c8b12
Myzwer <josh@onefortyfivedesign.com>
2022-12-01 18:58:42 -0500

    :white_check_mark: Added these files though they aren't in use right now.

---
+++ components/ProgressBarSingular.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="progress-bar">
+    <v-app id="doesitmatterstill">
+      <v-progress-linear
+        v-if="givingSingleAmount !== undefined"
+        v-model="percentConvert"
+        color="saltydog"
+        height="10"
+      />
+      <v-progress-linear v-else indeterminate color="saltydog" height="10" />
+    </v-app>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProgressBarSingular',
+  props: {
+    givingSingleAmount: {
+      type: Number,
+      required: false,
+      default: undefined,
+    },
+    givingSingleGoal: {
+      type: Number,
+      required: true,
+    },
+  },
+  computed: {
+    percentConvert() {
+      return (this.givingSingleAmount / this.givingSingleGoal) * 100
+    },
+  },
+}
+</script>
---
+++ components/SingleDisplayNumbers.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="text-left">
+    <div class="py-3">
+      <h2
+        v-if="givingSingleAmount !== undefined"
+        class="text-blue font-bold text-4xl"
+      >
+        {{
+          Number(givingSingleAmount).toLocaleString('en-US', {
+            style: 'currency',
+            currency: 'USD',
+          })
+        }}
+      </h2>
+      <v-progress-circular v-else indeterminate :size="40" class="text-blue" />
+
+      <p>
+        Given of
+        {{
+          Number(givingSingleGoal).toLocaleString('en-US', {
+            style: 'currency',
+            currency: 'USD',
+          })
+        }}
+      </p>
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SingleDisplayNumbers',
+  props: {
+    givingSingleAmount: {
+      type: Number,
+      required: false,
+      default: undefined,
+    },
+    givingSingleGoal: {
+      type: Number,
+      required: true,
+    },
+
+  },
+}
+</script>
