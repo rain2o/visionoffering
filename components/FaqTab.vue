0e225f5a70beec1dfc4cbee04f42d9e9262b264b
Myzwer <josh@onefortyfivedesign.com>
2023-10-19 11:08:45 -0400

    :script: Updated the FAQ copy

--- components/FaqTab.vue
+++ components/FaqTab.vue
@@ -1,79 +1,71 @@
 <template>
 
-  <div class="lg:max-w-2xl py-10">
-    <div class="grid grid-cols-12 gap-4">
-      <div class="col-span-12">
-        <h3 class=" uppercase bold text-2xl pb-5">FAQ</h3>
-        <hr>
-        <div class="py-5">
-          <h5 class = "uppercase font-bold text-xl">If we give $1,100,000, what will happen? </h5>
-          <p>Suppose the people of FC meet all three goals. In that case, we will (1) renovate our Knoxville location, (2) start our counseling ministry (3) our elders will diligently pursue the acquisition of three acres adjacent to our Maryville location.</p>
-        </div>
+	<div class="lg:max-w-2xl py-10">
+		<div class="grid grid-cols-12 gap-4">
+			<div class="col-span-12">
+				<h3 class=" uppercase bold text-2xl pb-5">FAQ</h3>
+				<hr>
+				<div class="py-5">
+					<h5 class="uppercase font-bold text-xl">If we give $1,694,500, what will happen?</h5>
+					<p>Suppose the people of FC meet all three goals. In that case, we will (1) bless our local and global outreach ministry partners, (2) have the funds prepared to purchase land and expand our Maryville location and (3) begin phase 2 of our Knoxville location renovation.</p>
+				</div>
 
-        <hr>
-        <div class="py-5">
-          <h5 class = "uppercase font-bold text-xl">Why is each goal in its current sequential order? </h5>
-          <p>Our elders believe that our most significant opportunity to make more disciples is by investing first in our Knoxville location. Research has shown that new churches grow faster, reach more people, and attract the lost quicker than existing & established churches in a community. Secondly, we know that counseling is a great need that can be met with minimal financial investment. Third, our Maryville location expansion is a significant long-term investment that will take further capital to produce its total result. </p>
-        </div>
+				<hr>
+				<div class="py-5">
+					<h5 class="uppercase font-bold text-xl">Why is each goal in its current sequential order? </h5>
+					<p>Our elders believe that our most significant opportunity to make more disciples is by investing in our outreach ministries first. Secondly, our Maryville location expansion is a significant long-term investment that will take further capital to produce its total result. Finally, we need to expand to adjust to the growing need for additional space at our Knoxville location. </p>
+				</div>
 
-        <hr>
-        <div class="py-5">
-          <h5 class = "uppercase font-bold text-xl">Will the $374,000 fully complete the Knoxville location? </h5>
-          <p>The resources given to the Vision Offering will complete phase one of our Knoxville location. Phase one allows the facility to be "move-in-ready." The first renovation segment will construct the atrium, connect center, a 200-seat auditorium, two kids' ministry spaces, and more. Future phases of renovation will occur as the Knoxville location grows. </p>
-        </div>
+				<hr>
+				<div class="py-5">
+					<h5 class="uppercase font-bold text-xl">Are we still building a physical counseling center? </h5>
+					<p>It is our future hope to build a counseling center. Unfortunately, we do not believe this is the best use of resources right now. Instead, we will use our current Maryville location to house our counseling ministry for the foreseeable future.</p>
+				</div>
 
-        <hr>
-        <div class="py-5">
-          <h5 class = "uppercase font-bold text-xl">Are we still building a physical counseling center? </h5>
-          <p>It is our future hope to build a counseling center. Unfortunately, we do not believe this is the best use of resources right now. Instead, we will use our current Maryville location to house our new counseling ministry for the foreseeable future. </p>
-        </div>
-
-        <hr>
-        <div class="py-5">
-          <h5 class = "uppercase font-bold text-xl">Why do we need counselors? Don't we have pastors? </h5>
-          <p>Our pastoral team regularly offers biblical wisdom & counsel. But, most of them are not classically trained in biblical counseling. We believe that to serve our community best, we need to invest in trained & licensed biblical counselors to deal with the many spiritual, relational, and emotional needs. </p>
-        </div>
-
-        <hr>
-        <div class="py-5">
-          <h5 class = "uppercase font-bold text-xl">Is the property surrounding the Maryville location on the market?</h5>
-          <p>The property is not currently for sale. Our elders are diligently pursuing the purchase of the property with the current owner. We pray that God would move in the owner's heart & would be willing to give or sell us the property at a fair market value.  </p>
-        </div>
-
-        <hr>
-        <div class="py-5">
-          <h5 class = "uppercase font-bold text-xl">Does the Maryville location expansion include the creation of a parking lot & counseling center?</h5>
-          <p>The $505,000 given will be for property acquisition. Plans to create space, pave the lot, and build a facility will occur in the future as the elders see fit. </p>
-        </div>
+				<hr>
+				<div class="py-5">
+					<h5 class="uppercase font-bold text-xl">IS THE PROPERTY SURROUNDING THE MARYVILLE LOCATION ON THE MARKET?</h5>
+					<p>The property is not currently for sale. Our elders are diligently pursuing the purchase of the property with the current owner. We pray that God would move in the owner's heart & would be willing to give or sell us the property at a fair market value.</p>
+				</div>
 
 
-        <hr>
-        <div class="py-5">
-          <h5 class = "uppercase font-bold text-xl">Can I give to just one specific goal? </h5>
-          <p>All donations to the Vision Offering will be designated to our general fund to pursue all three of our strategic goals. </p>
-        </div>
+				<hr>
+				<div class="py-5">
+					<h5 class="uppercase font-bold text-xl">Does the Maryville location expansion include the creation
+						of a parking lot & counseling center?</h5>
+					<p>The $600,000 given will be for property acquisition. Plans to create space, pave the lot, and build a facility will occur in the future as the elders see fit.</p>
+				</div>
 
 
-        <hr>
-        <div class="py-5">
-          <h5 class = "uppercase font-bold text-xl">If one goal is partially met, what happens? </h5>
-          <p>Our elders will determine the best use of the resources based on prayer & our financial position as church. </p>
-        </div>
+				<hr>
+				<div class="py-5">
+					<h5 class="uppercase font-bold text-xl">Can I give to just one specific goal? </h5>
+					<p>All donations to the Vision Offering will be designated to our general fund to pursue all three
+						of our strategic goals. </p>
+				</div>
 
 
-        <hr>
-        <div class="py-5">
-          <h5 class = "uppercase font-bold text-xl">Can I give stocks, securities, or crypto?</h5>
-          <p>Yes. To learn more, visit <a href="https://foothillschurch.com/give-stocks/">foothillschurch.com/give-stocks/</a>.</p>
-        </div>
-      </div>
-    </div>
-  </div>
+				<hr>
+				<div class="py-5">
+					<h5 class="uppercase font-bold text-xl">If one goal is partially met, what happens? </h5>
+					<p>Our elders will determine the best use of the resources based on prayer & our financial position
+						as church. </p>
+				</div>
+
+				<hr>
+				<div class="py-5">
+					<h5 class="uppercase font-bold text-xl">Can I give stocks, securities, or crypto?</h5>
+					<p>Yes. To learn more, visit <a href="https://foothillschurch.com/give-stocks/">foothillschurch.com/give-stocks/</a>.
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
 
 </template>
 
 <script>
 export default {
-  name: 'FaqTab'
+	name: 'FaqTab'
 }
 </script>
