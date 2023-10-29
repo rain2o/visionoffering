466a5560eaeaff89a996e6a892b71adf2868faaa
Myzwer <josh@onefortyfivedesign.com>
2023-10-29 07:28:58 -0400

    :bug: Forgot a class so the video didn't display right. This corrects that.

--- components/VideoPlayer.vue
+++ components/VideoPlayer.vue
@@ -1,12 +1,14 @@
 <template>
 	<div class="video-container">
 		<iframe
+			class="responsive-iframe"
 			width="560"
-			height="315" src="https://www.youtube.com/embed/sieLi1w164A?si=oGrWiFUR-UGasWqN"
+			height="315"
+			src="https://www.youtube.com/embed/sieLi1w164A?si=oGrWiFUR-UGasWqN"
 			title="YouTube video player"
-			frameborder="0"
-			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-			allowfullscreen></iframe>
+			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+			allowfullscreen>
+		</iframe>
 	</div>
 </template>
 
